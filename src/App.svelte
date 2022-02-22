327c783adff6e8867cc71ace0be968a8d40457d3
Evan Hecht <evanhecht01@gmail.com>
2022-02-22 02:45:13 -0600

    Initial commit, adding basics project structure and Svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import NavBar from "./svelte-components/NavBar.svelte";
+
+</script>
+
+<style>
+
+</style>
+
+<h1>Hello Bruh</h1>
+<NavBar/>
---
+++ src/svelte-components/NavBar.svelte
@@ -0,0 +1,9 @@
+<script>
+    let x = "peepeeddsdssddsdsdsddsds weiner"
+</script>
+
+<style>
+
+</style>
+
+<h1>Hello Winer {x}</h1>
